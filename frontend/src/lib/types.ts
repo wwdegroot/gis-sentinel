ca5632bd834e29d980314e8a0b6e9677c81e1836
wwdegroot <wouter.w.de.groot@gmail.com>
2025-01-02 21:38:37 +0100

    websocket alert message types

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,18 @@
+enum AlertType {
+    Update = "Update",
+    New = "New",
+    Remove = "Remove",
+}
+
+interface SentinelAlert {
+    id: string;
+    atype: AlertType,
+    name: string;
+    performance: number;
+    expected: number;
+    up: boolean;
+    reason: string;
+    error?: string;
+}
+
+export type { SentinelAlert, AlertType }
