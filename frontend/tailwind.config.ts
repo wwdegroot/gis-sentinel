5a196f95f16120daba75704a87f3c97186929c90
wwdegroot <wouter.w.de.groot@gmail.com>
2025-01-02 21:39:14 +0100

    container styling

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,6 +4,16 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem',
+			  },
+		},
 		extend: {}
 	},
 
